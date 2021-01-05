50ba15b03c0fd46a87a9174c47919b5cdde88567
AndrewColinMaher <colinmaher1710@yahoo.ie>
2021-01-05 20:06:32 +0000

    added js

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-$(document).ready(function () {
-
-
-    $("#stream1_btn").on(function () { //can use .click also
-        $(".stream1").slideDown();
-        $(".stream1").slideDown(1000);
-    });
-    $("#stream2_btn").on("click", function () {
-        $(".stream1").fadeTO(1000, 0.5);
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-    });
-    $("#stream3_btn").on("click", function () {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-    });
-
-
-});
-
 // $(document).ready(function () {
 
 
-//     $("#stream1_btn").mouseEnter(function () { //can use .click also
-//         $(".stream1").removeClass('highlight_stream');
-//         $(".stream2").removeClass('highlight_stream');
-//         $(".stream3").removeClass('highlight_stream');
-//         $(".stream1").addClass('highlight_stream');
+//     $("#stream1_btn").on(function () { //can use .click also
+//         $(".stream1").slideDown();
+//         $(".stream1").slideDown(1000);
 //     });
 //     $("#stream2_btn").on("click", function () {
-//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream1").fadeTO(1000, 0.5);
 //         $(".stream2").removeClass('highlight_stream');
 //         $(".stream3").removeClass('highlight_stream');
 //         $(".stream2").addClass('highlight_stream');
@@ -46,6 +21,31 @@ $(document).ready(function () {
 
 // });
 
+$(document).ready(function () {
+
+
+    $("#stream1_btn").mouseEnter(function () { //can use .click also
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+    });
+    $("#stream2_btn").on("click", function () {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+    });
+    $("#stream3_btn").on("click", function () {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+    });
+
+
+});
+
 
 // $("#button-card-1").mouseenter(function () {
 //     $("#par-1").fadeTo(200,0.5);
@@ -59,4 +59,12 @@ $(document).ready(function () {
 // });
 // $("#button-card-2").mouseleave(function () {
 //     $("#par-2").fadeTo(1000,1);
+// });
+
+// $("#button-card-1").click(function () {
+//     $("#par-1").hide(2000).show(2000);
+// });
+
+// $("#button-card-2").click(function () {
+//     $("#par-2").fadeOut(1000).fadeIn(1000);
 // });
